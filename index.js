c5087b0a813c4c0840c0b213d4c55545198c6a94
Fraasi <doofassi@gmail.com>
2020-02-21 03:34:14 +0200

    times

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const git_context = core.getInput('git_context');
+  console.log(`git_context ${git_context}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
